e16e3aff675a26e7b422887b6cb80c99d0dbf24c
AtharvaPingale <atharvapingale@gmail.com>
2020-03-25 18:21:19 +0530

    Added QR code

--- Screens/QrShow.js
+++ Screens/QrShow.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, StyleSheet, Text} from "react-native";
+import QRCode from 'react-native-qrcode-svg';
 
 export class QrShow extends Component {
   render() {
     return (
-      <View>
-        <View></View>
+      <View style={styles.qr}>
+        <QRCode
+          value="random string for testing"
+          size={250}
+          color="black"
+          backgroundColor="white"
+        />
+        <Text style={{marginTop:30, fontSize: 25}}>Scan this QR code</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  qr:{
+    alignContent:"center",
+    alignItems:"center",
+    marginTop:120
+  }
+});
 
 export default QrShow;
